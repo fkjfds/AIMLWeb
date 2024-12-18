bd7a8bd69af9e037e478fad847059371452c5ac5
fkjfds <ksy864464@gmail.com>
2024-12-18 13:39:51 +0900

    Add files via upload

--- Ch03/primaStreamlit.py
+++ Ch03/primaStreamlit.py
@@ -31,10 +31,120 @@ if uploaded_file is not None:
                 mean_value = int(data[col][data[col] != 0].mean())
             elif data[col].dtype == "float64":
                 mean_value = data[col].replace(0, mean_value)
-    #데이터 확인
-    buffer = io.StringIO()
-    data.info(buf=buffer)
-    info_ouput = buffer.getvalue()
-    st.text(info_ouput)
+    # 데이터 확인
+    # buffer = io.StringIO()
+    # data.info(buf=buffer)
+    # info_ouput = buffer.getvalue()
+    # st.text(info_ouput)
     st.write("Processed Data (0 values replaced with mean): ")
     st.write(data.describe())
+
+    # 데이터 분할
+
+    X = data.iloc[:, :-1] # 피처
+    y = data.iloc[:, -1] # 타겟, 레이블
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                        random_state=42, stratify=y)
+    
+    # 피처 중요도 분석
+    if st.checkbox("Show Features Importance"):
+        rf_model = RandomForestClassifier(random_state=42)
+        rf_model.fit(X_train, y_train)
+        feature_importance = pd.DataFrame({
+            "Feature" : X.columns,
+            "Importance" : rf_model.feature_importances_
+        }).sort_values(by="importance", ascending=False)
+
+        st.write(feature_importance)
+
+        # 피처 선택
+        selected_features = st.multiselect(
+            "Select Features for Prediction",
+            options = list(X.columns),
+            default = list(X.columns)
+        )
+
+        if selected_features:
+            X_train_selected = X_train[selected_features]
+            X_test_selected = X_test[selected_features]
+
+            # 분류기 선택
+            classifier_name = st.selectbox(
+                "Select Classifier",
+                ["Logistic Regression", "Random Forest", "Decision Tree"]
+            )
+            # 분류기 초기화
+            if classifier_name == "Logistic Regression":
+                model = LogisticRegression(solver="liblinear")
+            elif classifier_name == "Random Forest":
+                model = RandomForestClassifier()
+            elif classifier_name == "Decision Tree":
+                model = DecisionTreeClassifier()
+
+            # 모델학습
+            model.fit(X_train_selected, y_train)
+            y_scores = model.predict_proba(X_test_selected)[:, 1]
+
+            # Accuracy
+            y_pred = (y_scores > 0.5).astype(int) # true, false -> 1, 0
+            accuracy = accuracy_score(y_test, y_pred)
+            st.write(f"Accuracy of {classifier_name}: {accuracy:.2f}")
+
+            #Threshold 조정 슬라이더(message, min, max, default value, step)
+            threshold = st.slider("Adjust Threshold", 0.0, 1.0, 0.5, 0.01)
+
+            # Threshold에 따른 예측 및 성능 계산
+            y_pred_threshold = (y_scores > threshold).astype(int)
+            accuracy_threshold = accuracy_score(y_test, y_pred_threshold)
+            precision_threshold = precision_score(y_test, y_pred_threshold)
+            recall_threshold = recall_score(y_test, y_pred_threshold)
+
+            st.write(f"Performance with threshold {threshold:.2f}")
+            st.write(f"- Accuracy: {accuracy_threshold:.2f}")
+            st.write(f"- Precision: {precision_threshold:.2f}")
+            st.write(f"- Recall: {recall_threshold:.2f}")
+
+            # 정밀도-재현율 그래프
+            if st.checkbox("Show Precision-Recall vs Threshold Curve"):
+                precision, recall, threshold = precision_recall_curve(y_Test, y_scores)
+                plt.figure(figsize=(10, 6))
+                plt.plot(threshold, precision[:-1], label="recall", marker='.')
+                plt.plot(threshold, recall[:-1], label="Recall", marker='.')
+                plt.xlabel("Threshold");plt.ylabel("Precision / Recall")
+                plt.title("Precsion and Recall vs Threshold")
+                plt.legend();plt.grid()
+                start, end = plt.xlim()
+                plt.xtickes(np.arange(start, end, 0.1))
+                st.pyplot(plt)
+
+            # AUC-ROC 그래프
+            if st.checkbox("Show AUC-ROC Curve"):
+                fpr, tpr, threshold = roc_curve(y_test, y_scores)
+                roc_auc = auc(fpr, tpr)
+
+                plt.figure(figsize=(8, 6))
+                plt.plot(fpr,tpr, marker='.', label = f"AUC : {roc_auc:.2f}")
+                plt.xlabel("False Positive Rate");plt.ylabel("True Positive Rate")
+                plt.legend();plt.grid()
+                st.pyplot(plt)
+
+            # 사용자 입력값으로 예측
+            st.write("Make a New Prediction")
+            new_data = []
+
+            for feature in selected_features:
+                if data[feature].dtype == "float64":
+                    value = st.slider(f"Enter value for {feature}",
+                                      float(X[feature].min()),
+                                      float(X[feature].max()))
+                    new_data.append(value)
+                elif data[feature].dtype == "int64":
+                    value = st.slider(f"Enter value for {feature}",
+                                      int(X[feature].min()),
+                                      int(X[feature].max()))
+                    new_data.append(value)
+
+            if st.button("Predict"):
+                predicion = (model.predic_proba([new_data])[:, 1] > threshold).astype(int)
+                result = "Diabetic" if prediction[0] == 1 else "Non-diabtic"
+                st.write(f"Prediction with threshold : {result}")
